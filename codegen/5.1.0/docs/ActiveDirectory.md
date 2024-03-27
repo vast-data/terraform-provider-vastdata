2eda1f2f13cb5f55b00a4e574c99e46f6e7d177b
Tal Engel-Potlog <tal@vastdata.com>
2024-03-27 16:38:20 +0200

    Adding API files for 5.1.0 version

---
+++ codegen/5.1.0/docs/ActiveDirectory.md
@@ -0,0 +1,13 @@
+# ActiveDirectory
+
+## Properties
+Name | Type | Description | Notes
+------------ | ------------- | ------------- | -------------
+**Id** | **int32** |  | [optional] [default to null]
+**Guid** | **string** |  | [optional] [default to null]
+**MachineAccountName** | **string** |  | [optional] [default to null]
+**OrganizationalUnit** | **string** |  | [optional] [default to null]
+**LdapId** | **int64** |  | [optional] [default to null]
+
+[[Back to Model list]](../README.md#documentation-for-models) [[Back to API list]](../README.md#documentation-for-api-endpoints) [[Back to README]](../README.md)
+
--- vast_versions/versions_map.go
+++ vast_versions/versions_map.go
@@ -4,6 +4,7 @@ import (
 	version_4_6_0 "github.com/vast-data/terraform-provider-vastdata/codegen/4.6.0"
 	version_4_7_0 "github.com/vast-data/terraform-provider-vastdata/codegen/4.7.0"
 	version_5_0_0 "github.com/vast-data/terraform-provider-vastdata/codegen/5.0.0"
+	version_5_1_0 "github.com/vast-data/terraform-provider-vastdata/codegen/5.1.0"
 	"reflect"
 )
 
@@ -112,5 +113,41 @@ var vast_versions map[string]map[string]reflect.Type = map[string]map[string]ref
 		"ViewPolicy":                      reflect.TypeOf((*version_5_0_0.ViewPolicy)(nil)).Elem(),
 		"ViewShareAcl":                    reflect.TypeOf((*version_5_0_0.ViewShareAcl)(nil)).Elem(),
 		"VipPool":                         reflect.TypeOf((*version_5_0_0.VipPool)(nil)).Elem(),
+	},
+	"5.1.0": map[string]reflect.Type{
+		"ActiveDirectory":                 reflect.TypeOf((*version_5_1_0.ActiveDirectory)(nil)).Elem(),
+		"Cnode":                           reflect.TypeOf((*version_5_1_0.Cnode)(nil)).Elem(),
+		"DefaultQuota":                    reflect.TypeOf((*version_5_1_0.DefaultQuota)(nil)).Elem(),
+		"Dns":                             reflect.TypeOf((*version_5_1_0.Dns)(nil)).Elem(),
+		"GlobalLocalSnapshot":             reflect.TypeOf((*version_5_1_0.GlobalLocalSnapshot)(nil)).Elem(),
+		"GlobalSnapshot":                  reflect.TypeOf((*version_5_1_0.GlobalSnapshot)(nil)).Elem(),
+		"GlobalSnapshotOwnerRootSnapshot": reflect.TypeOf((*version_5_1_0.GlobalSnapshotOwnerRootSnapshot)(nil)).Elem(),
+		"GlobalSnapshotOwnerTenant":       reflect.TypeOf((*version_5_1_0.GlobalSnapshotOwnerTenant)(nil)).Elem(),
+		"Group":                           reflect.TypeOf((*version_5_1_0.Group)(nil)).Elem(),
+		"Ldap":                            reflect.TypeOf((*version_5_1_0.Ldap)(nil)).Elem(),
+		"Nis":                             reflect.TypeOf((*version_5_1_0.Nis)(nil)).Elem(),
+		"ProtectedPath":                   reflect.TypeOf((*version_5_1_0.ProtectedPath)(nil)).Elem(),
+		"ProtectionPolicy":                reflect.TypeOf((*version_5_1_0.ProtectionPolicy)(nil)).Elem(),
+		"ProtectionPolicySchedule":        reflect.TypeOf((*version_5_1_0.ProtectionPolicySchedule)(nil)).Elem(),
+		"QosDynamicLimits":                reflect.TypeOf((*version_5_1_0.QosDynamicLimits)(nil)).Elem(),
+		"QosPolicy":                       reflect.TypeOf((*version_5_1_0.QosPolicy)(nil)).Elem(),
+		"QosStaticLimits":                 reflect.TypeOf((*version_5_1_0.QosStaticLimits)(nil)).Elem(),
+		"Quota":                           reflect.TypeOf((*version_5_1_0.Quota)(nil)).Elem(),
+		"QuotaEntityInfo":                 reflect.TypeOf((*version_5_1_0.QuotaEntityInfo)(nil)).Elem(),
+		"ReplicationPeers":                reflect.TypeOf((*version_5_1_0.ReplicationPeers)(nil)).Elem(),
+		"S3LifeCycleRule":                 reflect.TypeOf((*version_5_1_0.S3LifeCycleRule)(nil)).Elem(),
+		"S3Policy":                        reflect.TypeOf((*version_5_1_0.S3Policy)(nil)).Elem(),
+		"S3replicationPeers":              reflect.TypeOf((*version_5_1_0.S3replicationPeers)(nil)).Elem(),
+		"ShareAcl":                        reflect.TypeOf((*version_5_1_0.ShareAcl)(nil)).Elem(),
+		"Snapshot":                        reflect.TypeOf((*version_5_1_0.Snapshot)(nil)).Elem(),
+		"Tenant":                          reflect.TypeOf((*version_5_1_0.Tenant)(nil)).Elem(),
+		"User":                            reflect.TypeOf((*version_5_1_0.User)(nil)).Elem(),
+		"UserKey":                         reflect.TypeOf((*version_5_1_0.UserKey)(nil)).Elem(),
+		"UserQuota":                       reflect.TypeOf((*version_5_1_0.UserQuota)(nil)).Elem(),
+		"View":                            reflect.TypeOf((*version_5_1_0.View)(nil)).Elem(),
+		"ViewCreate":                      reflect.TypeOf((*version_5_1_0.ViewCreate)(nil)).Elem(),
+		"ViewPolicy":                      reflect.TypeOf((*version_5_1_0.ViewPolicy)(nil)).Elem(),
+		"ViewShareAcl":                    reflect.TypeOf((*version_5_1_0.ViewShareAcl)(nil)).Elem(),
+		"VipPool":                         reflect.TypeOf((*version_5_1_0.VipPool)(nil)).Elem(),
 	},
 }
