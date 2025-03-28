bdbbab01e8b94b34207c3783340d1cbb5944d8d2
marek.czaplicki <marek.czaplicki@vastdata.com>
2025-03-28 17:07:56 +0100

    TERF-21: Implement a composite ID

--- utils/non_local_users_functions.go
+++ utils/non_local_users_functions.go
@@ -9,10 +9,32 @@ import (
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	vastclient "github.com/vast-data/terraform-provider-vastdata/vast-client"
 	"net/http"
+	"strconv"
+	"strings"
 )
 
 var nonLocalUserListsAttributes = []string{"s3_policies_ids"}
 var nonLocalUserBooleanAttributes = []string{"allow_delete_bucket", "allow_create_bucket"}
+
+func getNonLocalUserId(uid int, tenantId int) string {
+	return fmt.Sprintf("%v-%v", uid, tenantId)
+}
+
+func getNonLocalUserUidAndTenantId(id string) (int, int, error) {
+	split := strings.Split(id, "-")
+	if len(split) != 2 {
+		return 0, 0, fmt.Errorf("invalid NonLocalUser ID: %s", id)
+	}
+	uid, err := strconv.Atoi(split[0])
+	if err != nil {
+		return 0, 0, err
+	}
+	tenantId, err := strconv.Atoi(split[1])
+	if err != nil {
+		return 0, 0, err
+	}
+	return uid, tenantId, nil
+}
 
 func NonLocalUserBeforePatchFunc(m map[string]interface{}, i interface{}, ctx context.Context, d *schema.ResourceData) (map[string]interface{}, error) {
 	FieldsUpdate(ctx, nonLocalUserListsAttributes, d, &m)
@@ -31,7 +53,19 @@ func NonLocalUserCreateFunc(ctx context.Context, _client interface{}, attr map[s
 		return nil, marshallingError
 	}
 	tflog.Debug(ctx, fmt.Sprintf("Calling POST to path \"%v\"", attr))
-	return client.Patch(ctx, (*attributes)["path"], "application/json", bytes.NewReader(buffer), map[string]string{})
+	response, err := client.Patch(ctx, (*attributes)["path"], "application/json", bytes.NewReader(buffer), map[string]string{})
+	if err != nil {
+		return nil, err
+	}
+	responseBody := map[string]interface{}{}
+	err = UnmarshalBodyToMap(response, &responseBody)
+	if err != nil {
+		return nil, err
+	}
+	uid := data["uid"].(int)
+	tenantId := data["tenant_id"].(int)
+	responseBody["id"] = getNonLocalUserId(uid, tenantId)
+	return FakeHttpResponse(response, responseBody)
 }
 
 func NonLocalUserGetFunc(ctx context.Context, _client interface{}, attr map[string]interface{}, d *schema.ResourceData, headers map[string]string) (*http.Response, error) {
@@ -56,11 +90,18 @@ func NonLocalUserGetFunc(ctx context.Context, _client interface{}, attr map[stri
 		return nil, err
 	}
 	responseBody["tenant_id"] = tenantId // tenant_id is missing from the response
+	responseBody["id"] = getNonLocalUserId(uid, tenantId)
 	return FakeHttpResponse(response, responseBody)
 }
 
 func NonLocalUserUpdateFunc(ctx context.Context, _client interface{}, attr map[string]interface{}, data map[string]interface{}, d *schema.ResourceData, headers map[string]string) (*http.Response, error) {
-	data["uid"] = attr["id"]
+	id := attr["id"].(string)
+	uid, tenantId, err := getNonLocalUserUidAndTenantId(id)
+	if err != nil {
+		return nil, err
+	}
+	data["uid"] = uid
+	data["tenant_id"] = tenantId
 	return NonLocalUserCreateFunc(ctx, _client, attr, data, headers)
 }
 
