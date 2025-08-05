4a0b5ed3ee282dbc9d62852fbc124cc60f37257f
Volodymyr Boiko <volodymyr.boiko@vastdata.com>
2025-08-05 19:11:15 +0300

    required fields for datasources from query params

--- vastdata/folder_readonly.go
+++ vastdata/folder_readonly.go
@@ -4,6 +4,10 @@ package provider
 import (
 	"context"
 	"github.com/hashicorp/terraform-plugin-framework/attr"
+	rschema "github.com/hashicorp/terraform-plugin-framework/resource/schema"
+	"github.com/hashicorp/terraform-plugin-framework/resource/schema/int64planmodifier"
+	planmodifiers "github.com/hashicorp/terraform-plugin-framework/resource/schema/planmodifier"
+	"github.com/hashicorp/terraform-plugin-framework/resource/schema/stringplanmodifier"
 	is "github.com/vast-data/terraform-provider-vastdata/vastdata/internalstate"
 	"net/http"
 )
@@ -24,7 +28,25 @@ func (m *FolderReadOnly) NewResourceManager(raw map[string]attr.Value, schema an
 		raw,
 		schema,
 		&is.TFStateHints{
-			SchemaRef: FolderReadOnlySchemaRef,
+			TFStateHintsForCustom: &is.TFStateHintsForCustom{
+				Description: "Make a Folder Read-Only",
+				SchemaAttributes: map[string]any{
+					"path": rschema.StringAttribute{
+						Required:    true,
+						Description: "Path of the folder to be read-only.",
+						PlanModifiers: []planmodifiers.String{
+							stringplanmodifier.RequiresReplace(),
+						},
+					},
+					"tenant_id": rschema.Int64Attribute{
+						Required:    true,
+						Description: "ID of the tenant to which the folder belongs.",
+						PlanModifiers: []planmodifiers.Int64{
+							int64planmodifier.RequiresReplace(),
+						},
+					},
+				},
+			},
 		},
 	)}
 }
@@ -47,20 +69,34 @@ func (m *FolderReadOnly) API(rest *VMSRest) VastResourceAPIWithContext {
 	return rest.Folders
 }
 
-// TODO: implemente read/create/update/delete methods for FolderReadOnly
+func (m *FolderReadOnly) ReadDatasource(ctx context.Context, rest *VMSRest) (DisplayableRecord, error) {
+	searchParams := getSearchParams(ctx, m.tfstate, nil)
+	return rest.Folders.GetReadOnlyWithContext(ctx, searchParams)
+}
+
 func (m *FolderReadOnly) ReadResource(ctx context.Context, rest *VMSRest) (DisplayableRecord, error) {
-	return nil, nil
+	record, err := m.ReadDatasource(ctx, rest)
+	if err := ignoreStatusCodes(err, http.StatusBadRequest, http.StatusNotFound); err != nil {
+		return nil, err
+	}
+	return record, nil
 }
 
 func (m *FolderReadOnly) CreateResource(ctx context.Context, rest *VMSRest) (DisplayableRecord, error) {
-	return nil, nil
+	searchParams := getSearchParams(ctx, m.tfstate, nil)
+	record, err := rest.Folders.SetReadOnlyWithContext(ctx, searchParams)
+	return record, err
 }
 
 func (m *FolderReadOnly) UpdateResource(ctx context.Context, plan UpdateResource, rest *VMSRest) (DisplayableRecord, error) {
-	return nil, nil
-
+	planTfstate := plan.(*FolderReadOnly).tfstate
+	searchParams := getSearchParams(ctx, m.tfstate, planTfstate)
+	record, err := rest.Folders.SetReadOnlyWithContext(ctx, searchParams)
+	return record, err
 }
 
 func (m *FolderReadOnly) DeleteResource(ctx context.Context, rest *VMSRest) error {
-	return nil
+	searchParams := getSearchParams(ctx, m.tfstate, nil)
+	_, err := rest.Folders.DeleteReadOnly(searchParams)
+	return err
 }
