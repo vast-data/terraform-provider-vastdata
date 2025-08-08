e4c920bb9af59bbaed2f2bd468e78d87ee2c8eb2
Volodymyr Boiko <volodymyr.boiko@vastdata.com>
2025-08-08 16:12:21 +0300

    appropriate search criteria for searching non local groups by groupname

--- examples/data-sources/vastdata_active_directory/data-source.tf
+++ examples/data-sources/vastdata_active_directory/data-source.tf
@@ -6,3 +6,6 @@ data "vastdata_active_directory" "vastdb_active_directory_by_guid" {
   guid = "00000000-0000-0000-0000-000000000001"
 }
 
+data "vastdata_active_directory" "vastdb_active_directory1" {
+  machine_account_name = "machine_acc"
+}
