1e4fe8121ccc320ebbc66a4023d84d1daaeb3f00
Volodymyr Boiko <volodymyr.boiko@vastdata.com>
2026-06-15 02:29:22 +0300

    dns: added support of auto allocation (TERF-241)

--- examples/resources/vastdata_view/resource.tf
+++ examples/resources/vastdata_view/resource.tf
@@ -139,51 +139,82 @@ resource "vastdata_view" "vastdb_view" {
 
 # --------------------
 
-# View with NFSv4 triggers enabled
-data "vastdata_view_policy" "nfs4_policy" {
+# ignore:e2e - nfs4_triggers endpoint requires cluster-level feature enablement
+data "vastdata_view_policy" "vastdb_view_policy_default" {
   name = "default"
 }
 
-resource "vastdata_view" "nfs4_triggers_view" {
-  path                 = "/vastdb_view/nfs4"
+resource "vastdata_view" "vastdb_view" {
+  path                 = "/vastdb_view/nfs4-triggers"
+  policy_id            = data.vastdata_view_policy.vastdb_view_policy_default.id
   create_dir           = true
-  policy_id            = data.vastdata_view_policy.nfs4_policy.id
   protocols            = ["NFS4"]
   enable_nfs4_triggers = true
 }
 
 # --------------------
 
-# S3 view with CORS configuration
-data "vastdata_view_policy" "s3_cors_policy" {
-  name = "s3_default_policy"
-}
 
-resource "vastdata_user" "cors_bucket_owner" {
-  name              = "cors_owner"
+resource "vastdata_user" "vastdb_user" {
+  name              = "vastdb_user"
   local_provider_id = 1
 }
 
-resource "vastdata_view" "s3_cors_view" {
+data "vastdata_view_policy" "vastdb_view_policy_s3_default" {
+  name = "s3_default_policy"
+}
+
+resource "vastdata_view" "vastdb_view" {
   path         = "/vastdb_view/s3-cors"
   bucket       = "vastdb-s3-cors-bucket"
   create_dir   = true
-  bucket_owner = vastdata_user.cors_bucket_owner.name
-  policy_id    = data.vastdata_view_policy.s3_cors_policy.id
+  bucket_owner = vastdata_user.vastdb_user.name
+  policy_id    = data.vastdata_view_policy.vastdb_view_policy_s3_default.id
   protocols    = ["S3"]
 
   s3cors_configuration = {
     cors_rules = [
       {
-        allowed_methods = ["GET", "PUT", "POST"]
-        allowed_origins = ["https://example.com", "https://app.example.com"]
-        allowed_headers = ["Authorization", "Content-Type"]
-        expose_headers  = ["ETag", "x-amz-request-id"]
+        allowed_methods = ["GET", "PUT"]
+        allowed_origins = ["https://example.com"]
+      },
+    ]
+  }
+}
+
+# --------------------
+
+
+resource "vastdata_user" "vastdb_user" {
+  name              = "vastdb_user"
+  local_provider_id = 1
+}
+
+data "vastdata_view_policy" "vastdb_view_policy_s3_default" {
+  name = "s3_default_policy"
+}
+
+resource "vastdata_view" "vastdb_view" {
+  path         = "/vastdb_view/s3-cors-full"
+  bucket       = "vastdb-s3-cors-full-bucket"
+  create_dir   = true
+  bucket_owner = vastdata_user.vastdb_user.name
+  policy_id    = data.vastdata_view_policy.vastdb_view_policy_s3_default.id
+  protocols    = ["S3"]
+
+  s3cors_configuration = {
+    cors_rules = [
+      {
+        allowed_methods = ["GET", "PUT", "POST", "DELETE"]
+        allowed_origins = ["https://app.example.com", "https://admin.example.com"]
+        allowed_headers = ["Authorization", "Content-Type", "X-Requested-With"]
+        expose_headers  = ["ETag", "x-amz-request-id", "x-amz-id-2"]
         max_age_seconds = 3600
       },
       {
         allowed_methods = ["GET"]
         allowed_origins = ["*"]
+        max_age_seconds = 86400
       },
     ]
   }
